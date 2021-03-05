62be72f002b707f9e3f9a60cdab8de99f5bd827b
Stefan Pejic <stefan.pejic@syrmia.com>
2021-03-05 10:26:06 +0100

    Add support for selecting instructions.
    This also includes selecting simple add instruction and a test.

--- llvm/lib/Target/Mips/MCTargetDesc/MipsMCCodeEmitter.cpp
+++ llvm/lib/Target/Mips/MCTargetDesc/MipsMCCodeEmitter.cpp
@@ -124,6 +124,10 @@ bool MipsMCCodeEmitter::isMips32r6(const MCSubtargetInfo &STI) const {
   return STI.hasFeature(Mips::FeatureMips32r6);
 }
 
+bool MipsMCCodeEmitter::isNanoMips(const MCSubtargetInfo &STI) const {
+  return STI.getFeatureBits()[Mips::FeatureNanoMips];
+}
+
 void MipsMCCodeEmitter::EmitByte(unsigned char C, raw_ostream &OS) const {
   OS << (char)C;
 }
