8a719c95979aa85f2da6f2c4b7aff87cfcad6c6a
Stefan Pejic <stefan.pejic@syrmia.com>
2021-08-16 16:48:09 +0200

    Emit $zero instead of $0
    nanoMIPS GNU assembler doesn't support $0.

--- llvm/lib/Target/Mips/MipsAsmPrinter.cpp
+++ llvm/lib/Target/Mips/MipsAsmPrinter.cpp
@@ -520,7 +520,10 @@ bool MipsAsmPrinter::PrintAsmOperand(const MachineInstr *MI, unsigned OpNum,
     case 'z':
       // $0 if zero, regular printing otherwise
       if (MO.isImm() && MO.getImm() == 0) {
-        O << "$0";
+        if (Subtarget->hasNanoMips())
+          O << "$zero";
+        else
+          O << "$0";
         return false;
       }
       // If not, call printOperand as normal.
